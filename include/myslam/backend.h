939eeea77bfeb60ccc759c08ed3d9638a553ea3c
2080524785 <--get>
2024-03-21 06:40:51 +0800

    set backend.h

---
+++ include/myslam/backend.h
@@ -0,0 +1,48 @@
+#ifndef MYSALM_BACKEND_H
+#define MYSALM_BACKEND_H
+
+#include "myslam/common_include.h"
+
+namespace myslam{
+    class Map;
+
+    class Backend{
+        public:
+            EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
+            typedef std::shared_ptr<Backend> Ptr;
+
+            Backend();
+
+            void SetCameras(Camera::Ptr left, Camera::Ptr right){
+                cam_left_ = left;
+                cam_right_ = right;
+            }
+
+            void SetMap(Map::Ptr map){
+                map_ = map;
+            }
+
+            void UpdateMap(Map::Ptr map);
+
+            void Stop();
+
+        private:
+
+            // process
+            void BackendLoop();
+
+            void Optimize(Map::KeyframesType &keyframes, Map::LandmarksType &landmarks);
+
+            Map::Ptr map_;
+            std::thread backend_thread_;
+            std::mutex data_mutex_;
+
+            // 是一个同步原语，它允许线程等待某个条件成立。
+            std::condition_variable map_update_;
+
+            // 用于表示后端是否正在运行
+            std::atomic<bool> backend_running_;
+    }
+}
+
+#endif
