c52f84f291912898e7ddbf46b7c21bd5d8b6fab6
2080524785 <--get>
2024-03-25 19:13:10 +0800

    finish VO file

--- include/myslam/visual_odomentry.h
+++ include/myslam/visual_odomentry.h
@@ -12,6 +12,30 @@ namespace myslam{
     class VisualOdometry{
         public:
             EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
+            typedef std::shared_ptr<VisualOdometry> Ptr;
+
+            VisualOdometry(std::string &config_path);
+            
+            bool Init();
+
+            void Run();
+
+            bool Step();
+
+            FrontendStatus GetFrontendStatus() const {
+                return frontend_->GetStatus();
+            }
+        private:
+
+        bool inited_ = false;
+        std::string config_file_path_;
+
+        Frontend::Ptr frontend_ = nullptr;
+        Backend::Ptr backend_ = nullptr;
+        Map::Ptr map_ = nullptr;
+        Viewer::Ptr viewer_ = nullptr;
+
+        Dataset::Ptr dataset_ = nullptr;
     };
 }
 
