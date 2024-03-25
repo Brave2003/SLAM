81b5f799c855dd9d0b868789b9ebcdc756992303
2080524785 <--get>
2024-03-25 18:11:53 +0800

    finish viewer and create visual odomentry

---
+++ include/myslam/visual_odomentry.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef MYSLAM_VISUAL_ODOMETRY_H
+#define MYSLAM_VISUAL_ODOMETRY_H
+
+#include "myslam/backend.h"
+#include "myslam/common_include.h"
+#include "myslam/dataset.h"
+#include "myslam/frontend.h"
+#include "myslam/viewer.h"
+
+namespace myslam{
+    class VisualOdometry{
+        public:
+            EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
+    };
+}
+
+#endif
